978452413a647d7c010ec4bf3fcd4c51ab3f51ba
Thorsten Heydorn <theydorn@software-jensfiedler.de>
2023-10-24 14:57:49 +1000

    more complex actions

--- GCTest/MainPage.xaml.cs
+++ GCTest/MainPage.xaml.cs
@@ -15,11 +15,15 @@ namespace GCTest
 
             InitActions();
             BindableLayout.SetItemsSource(buttons, Actions);
+
+            InitActionsMoreComplex();
+            BindableLayout.SetItemsSource(buttonsMoreComplex, ActionsMoreComplex);
         }
 
         ~MainPage() => Debug.WriteLine("~MainPage");
 
         public ObservableCollection<ButtonAction> Actions { get; private set; }
+        public ObservableCollection<ButtonAction> ActionsMoreComplex { get; private set; }
 
         private void GCCollect(object sender, EventArgs e)
         {
@@ -40,6 +44,11 @@ namespace GCTest
                 {
                     Description = "FlexLayout",
                     Command = new Command(() => OpenContainerPage(() => new FlexLayout()))
+                },
+                new()
+                {
+                    Description = "Grid",
+                    Command = new Command(() => OpenContainerPage(() => new Grid()))
                 },
                 new()
                 {
@@ -235,6 +244,210 @@ namespace GCTest
                 {
                     Description = "ContentView",
                     Command = new Command(() => OpenContainerPage(() => new ContentView()))
+                },
+            ];
+        }
+
+        private void InitActionsMoreComplex()
+        {
+            ActionsMoreComplex =
+            [
+                new()
+                {
+                    Description = "AbsoluteLayout",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new AbsoluteLayout();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "Grid",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new Grid();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "FlexLayout",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new FlexLayout();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "HorizontalStackLayout",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new HorizontalStackLayout();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "StackLayout",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new StackLayout();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "VerticalStackLayout",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var layout = new VerticalStackLayout();
+                        layout.Add(new Label() { Text = "test" });
+                        return layout;
+                    }))
+                },
+                new()
+                {
+                    Description = "Border",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var border = new Border();
+                        border.Content = new Label() { Text = "test" };
+                        return border;
+                    }))
+                },
+                new()
+                {
+                    Description = "Frame",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var frame = new Frame();
+                        frame.Content = new Label() { Text = "test" };
+                        return frame;
+                    }))
+                },
+                new()
+                {
+                    Description = "Image",
+                    Command = new Command(() => OpenContainerPage(() => new Image() { Source = "dotnet_bot.png" }))
+                },
+                new()
+                {
+                    Description = "Label",
+                    Command = new Command(() => OpenContainerPage(() => new Label() { Text = "test" }))
+                },
+                new()
+                {
+                    Description = "ScrollView",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var view = new ScrollView();
+                        view.Content = new Label() { Text = "test" };
+                        return view;
+                    }))
+                },
+                new()
+                {
+                    Description = "WebView",
+                    Command = new Command(() => OpenContainerPage(() => new WebView() { Source = "https://google.com" }))
+                },
+                new()
+                {
+                    Description = "Ellipse",
+                    Command = new Command(() => OpenContainerPage(() => new Ellipse() { Stroke = Brush.Red, HeightRequest = 200, WidthRequest = 200 }))
+                },
+                new()
+                {
+                    Description = "Line",
+                    Command = new Command(() => OpenContainerPage(() => new Line(0, 0, 200, 200) { Stroke = Brush.Red, }))
+                },
+                new()
+                {
+                    Description = "Polygon",
+                    Command = new Command(() => OpenContainerPage(() => new Polygon(new PointCollection([
+                        new Point(0, 0),
+                        new Point(50, 100),
+                        new Point(200, 200)]))
+                    { Stroke = Brush.Red, }))
+                },
+                new()
+                {
+                    Description = "Polyline",
+                    Command = new Command(() => OpenContainerPage(() => new Polyline(new PointCollection([
+                        new Point(0, 0),
+                        new Point(50, 100),
+                        new Point(200, 200)]))
+                    { Stroke = Brush.Red, }))
+                },
+                new()
+                {
+                    Description = "Rectangle",
+                    Command = new Command(() => OpenContainerPage(() => new Rectangle() { Stroke = Brush.Red, WidthRequest = 200, HeightRequest = 200 }))
+                },
+                new()
+                {
+                    Description = "Button",
+                    Command = new Command(() => OpenContainerPage(() => new Button() { Text = "test" }))
+                },
+                new()
+                {
+                    Description = "ImageButton",
+                    Command = new Command(() => OpenContainerPage(() => new ImageButton() { Source = "dotnet_bot.png" }))
+                },
+                new()
+                {
+                    Description = "RefreshView",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var view = new RefreshView();
+                        view.Content = new Label() { Text = "test" };
+                        return view;
+                    }))
+                },
+                new()
+                {
+                    Description = "CarouselView",
+                    Command = new Command(() => OpenContainerPage(() => new CarouselView()
+                    { ItemsSource = new List<int>() { 1, 2, 3, 4, 5 } }))
+                },
+                new()
+                {
+                    Description = "CollectionView",
+                    Command = new Command(() => OpenContainerPage(() => new CollectionView()
+                    { ItemsSource = new List<int>() { 1, 2, 3, 4, 5 } }))
+                },
+                new()
+                {
+                    Description = "IndicatorView",
+                    Command = new Command(() => OpenContainerPage(() => new IndicatorView()))
+                },
+                new()
+                {
+                    Description = "ListView",
+                    Command = new Command(() => OpenContainerPage(() => new ListView()
+                    { ItemsSource = new List<int>() { 1, 2, 3, 4, 5 } }))
+                },
+                new()
+                {
+                    Description = "Picker",
+                    Command = new Command(() => OpenContainerPage(() => new Picker()
+                    { ItemsSource = new List<int>() { 1, 2, 3, 4, 5 } }))
+                },
+                new()
+                {
+                    Description = "ContentView",
+                    Command = new Command(() => OpenContainerPage(() =>
+                    {
+                        var view = new ContentView();
+                        view.Content = new Label() { Text = "test" };
+                        return view;
+                    }))
                 },
             ];
         }
